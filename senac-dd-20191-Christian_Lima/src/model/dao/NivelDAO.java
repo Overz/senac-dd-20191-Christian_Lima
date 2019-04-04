50c7312204cbd1afd72f9920a08d165dfb43aa21
Aluno <Aluno@S202M05>
2019-04-04 13:32:55 -0300

    .

---
+++ senac-dd-20191-Christian_Lima/src/model/dao/NivelDAO.java
@@ -0,0 +1,47 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import model.vo.NivelVO;
+
+public class NivelDAO {
+
+	/**
+	 * Retorna um nível, dado o seu id
+	 * @param idNivel
+	 * @return nivel
+	 */
+	public NivelVO consultarPorId(int idNivel) {
+		// TODO Auto-generated method stub
+		//SELECT * FROM NIVEL WHERE ID = idNivel
+
+		String query = "SELECT * FROM NIVEL WHERE id = " + idNivel;
+		
+		Connection conn = Banco.getConnection();
+		Statement stmt = Banco.getStatement(conn);
+		ResultSet resultado = null;
+		NivelVO nivelVO = null;
+		try {
+			resultado = stmt.executeQuery(query);
+			while (resultado.next()) {
+				nivelVO = new NivelVO();
+				
+				nivelVO.setId(resultado.getInt("id"));
+				nivelVO.setDescricao(resultado.getString("descricao"));
+				
+			}
+		
+		} catch (SQLException e) {
+			System.out.println("Erro ao obter resultados da tabela Usuario junto a Nivel. Erro: " + e.getMessage());
+		} finally {
+			Banco.closePreparedStatement(stmt);
+			Banco.closeConnection(conn);
+		}
+
+		return nivelVO;
+	}
+
+}
